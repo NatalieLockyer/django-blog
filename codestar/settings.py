68a25cb8aa02a4d95e36323ab680ab92688353f8
Natalie Lockyer <Natalie_lockyer@outlook.com>
2024-02-15 16:30:13 +0000

    Secret key added

--- codestar/settings.py
+++ codestar/settings.py
@@ -16,6 +16,7 @@ import dj_database_url
 if os.path.isfile('env.py'):
     import env
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k1cj)-(_3e@##q%_!o-bxlxatj!zku(+c64a5js!#+)y#8^al6'
+SECRET_KEY = os.environ.get("dUJoG25YKgcxaYJJWutdVMl5pG8PcvG1")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
