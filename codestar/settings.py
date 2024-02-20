34dd255a4c22c1caea3d9e6ba57d6c0a7a0c2477
Natalie Lockyer <Natalie_lockyer@outlook.com>
2024-02-20 09:36:52 +0000

    Adds a Post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -91,6 +91,12 @@ DATABASES = {
     "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeanyapp.com",
+    "https://*.herokuapp.com",
+    "https://*.gitpod.io"
+]
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
