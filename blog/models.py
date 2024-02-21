3f272cc7b92834c057384a0b8c9f4c1e3b4b999d
Natalie Lockyer <Natalie_lockyer@outlook.com>
2024-02-21 10:02:00 +0000

    Debugging

--- blog/models.py
+++ blog/models.py
@@ -17,9 +17,23 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
 
 
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} | {self.author}"
+
+
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        ordering: ["-created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
