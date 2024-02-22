ecb50d2ec677c43b36e24a9811d637fc0d7aa32c
Natalie Lockyer <Natalie_lockyer@outlook.com>
2024-02-22 12:59:20 +0000

    Added filter options

--- codestar/urls.py
+++ codestar/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path("", include("blog.urls"), name="blog-urls"),
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
+    path("", include("blog.urls"), name="blog-urls"),
+    
 ]
