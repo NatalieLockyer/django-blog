4c8318fc0270375e77027701d29252cd4b547d7f
Natalie Lockyer <Natalie_lockyer@outlook.com>
2024-02-23 14:17:32 +0000

    Update

--- about/models.py
+++ about/models.py
@@ -3,8 +3,9 @@ from django.db import models
 # Create your models here.
 class About(models.Model):
     title = models.CharField(max_length=50)
-    content = models.TextField()
     updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    
 
     def __str__(self):
         return self.title
