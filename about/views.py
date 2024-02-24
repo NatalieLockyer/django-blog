e7324f865b2835433c6a9e2f4dbcc5fba4595db9
Natalie Lockyer <Natalie_lockyer@outlook.com>
2024-02-24 15:16:29 +0000

    Collaborate form added

--- about/views.py
+++ about/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
-# Create your views here.
+from .forms import CollaborateForm
 
 
 def about_me(request):
-    """
-    Renders the About page
-    """
+
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(request, messages.SUCCESS, "Collaboration request received! I endeavour to respond within 2 working days.")
+
     about = About.objects.all().order_by('-updated_on').first()
+    collaborate_form = CollaborateForm()
 
     return render(
         request,
         "about/about.html",
-        {"about": about},
-    )   
+        {
+            "about": about,
+            "collaborate_form": collaborate_form
+        },
+    )
